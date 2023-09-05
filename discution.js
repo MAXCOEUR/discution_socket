8ac2b63401fbc8c7ce347f778cc9f6b26969378d
Maxence COEUR <maxence.chachane@gmail.com>
2023-09-05 21:34:43 +0200

    Update discution.js

--- discution.js
+++ discution.js
@@ -40,9 +40,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Autoriser toutes les origines
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Autoriser les méthodes
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Autoriser les en-têtes
-  next();
-});
-app.use('/api',(req, res, next) => {
   res.header('Content-Type', 'application/json; charset=utf-8'); // Spécifier l'encodage UTF-8
   next();
 });
