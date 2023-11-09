8c6239d20ea76def40effff4c8e9345877e0c348
MAXCOEUR <45947596+MAXCOEUR@users.noreply.github.com>
2023-11-09 11:11:03 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,13 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'maxence',
   password: 'Max2003?',
   database: 'discution'
 });
 
-db.connect(err => {
-  if (err) {
-    console.error('Erreur de connexion à la base de données:', err);
-  } else {
-    console.log('Connecté à la base de données MariaDB');
-  }
-});
+// Pour obtenir une promesse à partir du pool, vous pouvez utiliser la méthode promise()
+const db = pool.promise();
 
 module.exports = {db};
